18d910955d08a71563144242641cad6730c59d61
KETLABS <p-pleasai@hotmail.com>
2020-11-25 01:32:06 +0100

    main page FFU

---
+++ index.js
@@ -0,0 +1,16 @@
+// Open full article.
+
+function readArc1() {
+  document.getElementById('article1').style.height = 'auto';
+  document.getElementById('arcReadbt1').style.display = 'none';
+}
+
+function readArc2() {
+  document.getElementById('article2').style.height = 'auto';
+  document.getElementById('arcReadbt2').style.display = 'none';
+}
+
+function readArc3() {
+  document.getElementById('article3').style.height = 'auto';
+  document.getElementById('arcReadbt3').style.display = 'none';
+}
